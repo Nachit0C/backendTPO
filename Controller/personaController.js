56f6b9cd7d99a8a34771be0c612fa4c8aceec3ce
Ignacio Ciccone <150193559+Nachit0C@users.noreply.github.com>
2024-07-04 18:16:36 -0300

    Update personaController.js

--- Controller/personaController.js
+++ Controller/personaController.js
@@ -1,7 +1,7 @@
 const db = require('../database/db');
 
 const personas = (req, res) => {
-    const sql = `SELECT * FROM urioplata.personas;`;
+    const sql = `SELECT * FROM personas;`;
     db.query(sql, (err, result) =>{
         if(err) throw err;
         
@@ -15,7 +15,7 @@ const personas = (req, res) => {
 
 const getPersona = (req, res) => {
     const persona_id = req.params.id;
-    const sql = `SELECT * FROM urioplata.personas WHERE persona_id = ? ;`;
+    const sql = `SELECT * FROM personas WHERE persona_id = ? ;`;
     db.query(sql, persona_id, (err, result) =>{
         if(err) throw err;
 
@@ -32,14 +32,14 @@ const createPersona = (req, res) => {
         if(validarDatos(dataRequest)){
         const {nombre, apellido, dni, fecha_nacimiento, email, telefono, direccion}= dataRequest ;
         //Verifico si ya existe una persona con el mismo dni o email
-        const sql1 = `SELECT * FROM urioplata.personas WHERE (dni = ?) OR (email = ?);`;
+        const sql1 = `SELECT * FROM personas WHERE (dni = ?) OR (email = ?);`;
         db.query(sql1,[dni,email], (err,result) => {
             if (err) throw err;
 
             if (result.length > 0) {
                 return res.send('Ya existe una persona con ese dni o email.');
             } else {
-                const sql = `INSERT INTO urioplata.personas (nombre, apellido, dni, fecha_nacimiento, email, telefono, direccion) 
+                const sql = `INSERT INTO personas (nombre, apellido, dni, fecha_nacimiento, email, telefono, direccion) 
                     VALUES (?, ?, ?, ?, ?, ?, ?);`;
                 db.query(sql,[nombre, apellido, dni, fecha_nacimiento, email, telefono, direccion] ,(err, result) => {
                     if (err) throw err;
@@ -60,7 +60,7 @@ const updatePersona = (req, res) => {
     if(validarDatos(dataRequest)){
         const {nombre, apellido, dni, fecha_nacimiento, email, telefono, direccion}= dataRequest ;
         //Verifico que exista persona con el id enviado
-        const sql1 = `SELECT * FROM urioplata.personas WHERE persona_id = ? ;`;
+        const sql1 = `SELECT * FROM personas WHERE persona_id = ? ;`;
         db.query(sql1, persona_id, (err, result) =>{
             if(err) throw err;
 
@@ -68,14 +68,14 @@ const updatePersona = (req, res) => {
                 return res.send('No existe persona con ese id.');
             } else {
                 //Verifico si ya existe una persona con el mismo dni o email
-                const sql2 = `SELECT * FROM urioplata.personas WHERE (dni = ? OR email = ?) AND (persona_id != ?);`;
+                const sql2 = `SELECT * FROM personas WHERE (dni = ? OR email = ?) AND (persona_id != ?);`;
                 db.query(sql2,[dni,email], (err,result) => {
                     if (err) throw err;
 
                     if (result.length > 0) {
                         return res.send('Ya existe una persona con ese dni o email.');
                     } else {
-                        const sql3 = `UPDATE urioplata.personas SET nombre = ?, apellido = ?, dni = ?, fecha_nacimiento = ?, email = ?, telefono    = ?, direccion = ? WHERE (persona_id = ? );`;
+                        const sql3 = `UPDATE personas SET nombre = ?, apellido = ?, dni = ?, fecha_nacimiento = ?, email = ?, telefono    = ?, direccion = ? WHERE (persona_id = ? );`;
 
                         db.query(sql3,[nombre, apellido, dni, fecha_nacimiento, email, telefono, direccion, persona_id] ,(err, result) => {
                             if (err) throw err;
@@ -94,21 +94,21 @@ const deletePersona = (req, res) => {
     const persona_id = req.params.id;
 
     //Verifico que exista persona con el id enviado
-    const sql1 = `SELECT * FROM urioplata.personas WHERE persona_id = ? ;`;
+    const sql1 = `SELECT * FROM personas WHERE persona_id = ? ;`;
     db.query(sql1, persona_id, (err, result) =>{
         if(err) throw err;
 
         if (result.length == 0) {
             return res.status(400).send('No existe persona con ese id.');
         } else {
-            const sql2 = `SELECT alumno_id FROM urioplata.alumnos where persona_id= ? ;`;
+            const sql2 = `SELECT alumno_id FROM alumnos where persona_id= ? ;`;
             db.query(sql2, persona_id, (err, result) => {
                 if (err) throw err;
 
                 const alumno_id = result[0].alumno_id;
-                const sql1 = `DELETE FROM urioplata.inscripciones WHERE (alumno_id = ?);
-                DELETE FROM urioplata.alumnos WHERE (alumno_id = ?);
-                DELETE FROM urioplata.personas WHERE (persona_id = ?);`;
+                const sql1 = `DELETE FROM inscripciones WHERE (alumno_id = ?);
+                DELETE FROM alumnos WHERE (alumno_id = ?);
+                DELETE FROM personas WHERE (persona_id = ?);`;
 
                 db.query(sql1, [alumno_id,alumno_id,persona_id], (err, result) => {
                     if (err) throw err;
