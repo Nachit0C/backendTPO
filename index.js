f0ce7dceb6e2a5967d7245498414aa57c985fec8
Ignacio Ciccone <150193559+Nachit0C@users.noreply.github.com>
2024-07-04 16:52:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const myExpress = require('express');
 const blankRoute = require('./Routes/blankRoute');
-const carreras = require('./Routes/carreras');
-const sedes = require('./Routes/sedes');
-const persona = require('./Routes/persona');
-const alumno = require('./Routes/alumno');
-const inscripciones = require('./Routes/inscripciones');
+//const carreras = require('./Routes/carreras');
+//const sedes = require('./Routes/sedes');
+//const persona = require('./Routes/persona');
+//const alumno = require('./Routes/alumno');
+//const inscripciones = require('./Routes/inscripciones');
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,15 +14,15 @@ myApp.use(myExpress.json());
 
 myApp.use("/", blankRoute);
 
-myApp.use("/carreras", carreras);
+//myApp.use("/carreras", carreras);
 
-myApp.use("/sedes", sedes);
+//myApp.use("/sedes", sedes);
 
-myApp.use("/persona", persona);
+//myApp.use("/persona", persona);
 
-myApp.use("/alumno", alumno);
+//myApp.use("/alumno", alumno);
 
-myApp.use('/inscripciones', inscripciones);
+//myApp.use('/inscripciones', inscripciones);
 
 myApp.listen( PORT, () =>{
     console.log(`Server running at port: http://localhost:${PORT} .`);
